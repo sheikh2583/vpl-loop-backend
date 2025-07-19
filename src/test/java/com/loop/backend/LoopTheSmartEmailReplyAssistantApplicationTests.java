2d558c7e13c777c56aa798915a86c68ac608254f
Sheikh Mosheul Akbar <sheikhakbar@iut-dhaka.edu>
2025-07-19 18:28:54 +0600

    Initial Commit

---
+++ src/test/java/com/loop/backend/LoopTheSmartEmailReplyAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loop.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoopTheSmartEmailReplyAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
