05c1eb3c14d650087157b5e3592accb90ab30120
Sheikh Mosheul Akbar <sheikhakbar@iut-dhaka.edu>
2025-07-22 16:14:23 +0600

    Auto-commit on 22/07/2025 16:14:23.79

--- loop-frontend/src/App.jsx
+++ loop-frontend/src/App.jsx
@@ -1,34 +1,99 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
+import { Button, CircularProgress, FormControl, TextField, Typography } from '@mui/material';
+import { Container, Box } from '@mui/material';
+import { InputLabel, Select, MenuItem } from '@mui/material';
+import axios from 'axios';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[emailContent, setEmailContent]= useState('');
+  const[tone, setTone]= useState('');
+  const[generatedReply, setGeneratedReply]= useState('');
+  const[loading, setLoading]= useState(false);
+  const[error, setError]=useState('');
 
+  const handleSubmit =async()=> {
+    setLoading(true);
+    setError('');
+    try {
+      const response= await axios.post("http://localhost:8080/api/email/generate", {
+        emailContent,
+        tone
+      });
+      setGeneratedReply(typeof response.data=='string' ? response.data : JSON.stringify(response.data));
+    } catch (error) {
+      setError('Failed to generate email reply. Please try again');
+      console.error(error);
+    }finally{
+      setLoading(false);
+    }
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container maxWidth="md" sx={{py:4}}>
+      <Typography variant = 'h3' component ="h1" gutterBottom>
+        Email Reply Generator
+      </Typography>
+      <Box sx={{mx:3}}>
+        <TextField
+        fullWidth
+        multiline
+        rows={6}
+        variant='outlined'
+        label="Original Email Content"
+        value={emailContent || ''}
+        onChange={(e)=>setEmailContent(e.target.value)}
+        sx={{mb:2}}
+        />
+        <FormControl fullWidth sx={{mb:2}}>
+          <InputLabel>Tone (Optional)</InputLabel>
+          <Select 
+          value={tone || ''}
+          label={"Tone (Optional)"}
+          onChange={(e)=> setTone(e.target.value)}>
+              <MenuItem value="">None</MenuItem>   
+              <MenuItem value="profssional">Professional</MenuItem>     
+              <MenuItem value="casual">Casual</MenuItem>     
+              <MenuItem value="friendly">Friendly</MenuItem>       
+            </Select>
+        </FormControl>
+
+        <Button
+        variant='contained'
+        onClick={handleSubmit}
+        disabled={!emailContent || loading}
+        fullWidth>
+          {loading ? <CircularProgress size={24}/> : "Generate Reply"}
+        </Button>
+      </Box>
+      {error && (
+        <Typography color = 'error' sx={{mb:2}}>
+        {error}
+      </Typography>
+      )}
+      {generatedReply &&(
+        <Box sx={{mt:3}}>
+          <Typography variant='h6' gutterBottom>
+            Generated Reply:
+          </Typography>
+          <TextField
+          fullWidth
+          multiline
+          rows={6}
+          variant='outlined'
+          value={generatedReply || ''}
+          inputProps={{readOnly: true}}
+          />
+          <Button
+            variant='outlined'
+            sx={{mt:2}}  
+            onClick={()=> navigator.clipboard.writeText(generatedReply)}
+          >
+            Copy to Clipboard
+          </Button>
+        </Box>
+      )}
+      </Container>
   )
 }
 
